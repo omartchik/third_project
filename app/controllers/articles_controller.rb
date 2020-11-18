58f47d39440b4c9e5adeb9b44c18c016b504d83f
omar rachedi <omartchik@gmail.com>
2020-11-18 23:11:52 +0100

    simple form edit destroy...

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,21 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 
   private
 
