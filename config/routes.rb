5f74245d7556335e423e9b9b6cdbaa68e0545b0e
omar rachedi <omartchik@gmail.com>
2020-11-18 01:29:05 +0100

    add validations for article, change some routes, implement new and create article actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+  # get "restaurants" => "restaurants#index" # lister tous les articles
+  # get "restaurants/:id" => "restaurants#show"# afficher un seul article
+  # get "restaurants/new" => "restaurants#new"# formulaire de création d'un article
+  # post "resturants" => "restaurants#create"#creation d'un article
+  # get "restaurants/edit" => "restaurants#edit"# formulaire d'édition
+  # patch "restaurants/:id" => "restaurants#update"# mise à jour de l'article
+  # delete "restaurants/:id" => "restaurants#destroy"# suppression d'un article
 end
